5721240727d731f5efd8038e2abae94cc4e03893
Sanjay9745 <sanjysanthosh919@gmail.com>
2024-08-15 15:24:17 +0530

    new thing

--- browserSync.js
+++ browserSync.js
@@ -1,6 +1,7 @@
 const browserSync = require('browser-sync').create();
 
 browserSync.init({
-  proxy: "https://vpnmodel.onrender.com", // Your app URL
-  port: 4999, // BrowserSync port
-});
+    proxy: "https://vpnmodel.onrender.com", // Your app URL
+    port: 4999, // BrowserSync port
+    open: false, // Do not open a browser
+  });
